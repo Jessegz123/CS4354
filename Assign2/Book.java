c1090dace639440593b61cfb231cc1a689540a51
Jessegz123 <y_g59@txstate.edu>
2016-07-16 15:04:21 -0500

    Update Book.java

--- Assign2/Book.java
+++ Assign2/Book.java
@@ -1,7 +1,15 @@
 package store;
 /**
+@author Sarah Zsohar
+@author Jesse Gonzalez
+*/
+
+/**
    SubClass
  */
 public class Book {
-
+  private int isbs;
+  private String author;
+  private double shippingCredit = 3.99;
+  private double commmission = .15;
 }
