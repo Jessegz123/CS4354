5df4dee9b780ef79c0567aaff1905f5131d967f9
Jessegz123 <y_g59@txstate.edu>
2016-07-16 10:21:55 -0500

    Update Product.java

--- Assign2/Product.java
+++ Assign2/Product.java
@@ -1,5 +1,8 @@
 package store;
 
 public class Product {
-
+  private int sku;			//holds the SKU of the product
+  private int quantity;		//hold the quantity of the product
+  private double price;		//hold the price of the product
+  private String title; 	//hold the title of the product
 }
