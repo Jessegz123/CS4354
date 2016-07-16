99380ec81df9ab98751f0daf4d820f0f29714afc
Jessegz123 <y_g59@txstate.edu>
2016-07-16 10:37:39 -0500

    Update Driver.java

--- Assign2/Driver.java
+++ Assign2/Driver.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 public class Driver {
  
   /**
-  * Creates a new movie inventory
+  * Creates a new product inventory
   */
   private static Inventory inventory = new Inventory();
 
@@ -19,22 +19,22 @@ public class Driver {
 	int choice;
 	       
     do {
-      System.out.print("1. Add a movie to the inventory \n");
-	  System.out.print("2. Remove a movie from the inventory \n");
-	  System.out.print("3. Display the information for a movie \n");
+      System.out.print("1. Add a product to the inventory \n");
+	  System.out.print("2. Remove a product from the inventory \n");
+	  System.out.print("3. Display the information for a product \n");
 	  System.out.print("4. Display the inventory in a table \n");
 	  System.out.print("5. Quit \n\n");
 	  System.out.print("Enter your choice: \n");
 	  
 	  choice = input.nextInt();
 		      
-	  if (choice == 1) {
+	  /**if (choice == 1) {
 	    System.out.println("Add a Product (Enter M for Movie, B for book, or T for Toy:");
 	    String item = input.nextLine();
 	    
-	    inventory.addProduct(item);
-	  }
-      /*if (choice == 1) {
+	    //inventory.addProduct(item);
+	  }**/
+      if (choice == 1) {
 	    System.out.println("SKU:");
 	    int sku = input.nextInt();			//declare SKU
         System.out.println ("Quantity");
@@ -46,28 +46,28 @@ public class Driver {
 	    System.out.println ("Title: ");
 	    String title = input.nextLine();		//declare title
 					
-	    inventory.addMovie(sku, quantity, price, title);
+	    inventory.addProduct(sku, quantity, price, title);
 	  }
-      }*/
+      
   
       if (choice == 2) {
         //Scanner input = new Scanner (System.in);
         System.out.print("Enter SKU to remove: \n");
 	    int removeSku = input.nextInt();	//declare SKU to pass as parameter
-        inventory.removeMovie(removeSku);
+        inventory.removeProduct(removeSku);
       }
     
-	  if (choice == 3) {
+          if (choice == 3) {
 	    //Scanner input = new Scanner (System.in);
 	    System.out.print("Enter SKU to display: \n");
 	    int displaySku = input.nextInt();		//declare SKU to pass as parameter
-	    inventory.displayMovieInfo(displaySku);
+	    inventory.displayProductInfo(displaySku);
 	  }
 	  
 	  if (choice == 4) {
-	    inventory.displayAllMovies();
+	    inventory.displayAllProducts();
 	  }
-    } while (choice != 6);
+    } while (choice != 5);
 
     inventory.writeToFile();
     System.out.print("Program Ended");
